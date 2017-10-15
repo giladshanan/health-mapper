4e9c5c19cd3efa444a79c3eaafa520ec170b79f5
Gilad Shanan <giladshanan@gmail.com>
2017-10-15 11:35:25 -0500

    update testing and development gems, add routes, model, templates and actions for basic authentication

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :email, :password_digest, presence: true
+  validates :password, length: { minimum: 8 }
+end
